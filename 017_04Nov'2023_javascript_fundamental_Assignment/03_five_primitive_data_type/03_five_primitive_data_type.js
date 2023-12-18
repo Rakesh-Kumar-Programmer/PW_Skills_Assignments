bc38f94b875594308d6f4862e4d2124087f71a56
Rakesh Kumar <134847306+Rakesh-Kumar-Programmer@users.noreply.github.com>
2023-12-18 07:31:32 +0530

    04Nov'2023_javascript_fundamental_Assignment_Completed

---
+++ 017_04Nov'2023_javascript_fundamental_Assignment/03_five_primitive_data_type/03_five_primitive_data_type.js
@@ -0,0 +1,28 @@
+// 6 primitive data type
+
+// Numbar
+let intNumber = 10;
+
+// String
+let strName = "Rakesh";
+
+// Boolean
+let boolName = true;
+let boolName1 = false;
+
+// bigint
+let bigNumber = 123356467578687n
+
+//Undefined
+let unName // this unName is undifined
+
+//Null
+let value= null;
+
+
+console.log(typeof intNumber,typeof strName,typeof boolName,typeof bigNumber,typeof unName,value);
+
+
+
+
+
